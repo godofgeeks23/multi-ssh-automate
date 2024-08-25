ccee5041e7d7763bed5fe35719eb46c5a5e189c2
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-08-25 22:06:52 +0530

    Create automator.sh

---
+++ automator.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Extract aliases and corresponding SSH commands directly from the .env file
+mapfile -t servers < <(grep -oP '^[a-zA-Z0-9_-]+=".*"' .env)
+
+for entry in "${servers[@]}"; do
+    # Extract alias name and command
+    alias_name=$(echo "$entry" | cut -d '=' -f 1)
+    server_command=$(echo "$entry" | cut -d '"' -f 2)
+
+    echo "SSHing into $alias_name..."
+    echo "----------------------------------------"
+
+    # Run the SSH command with the corresponding server
+    $server_command <<EOF
+    echo "SSHed into $alias_name"
+    sudo docker ps
+    logout
+EOF
+
+    echo "Finished running commands on $alias_name"
+    echo "----------------------------------------"
+done
